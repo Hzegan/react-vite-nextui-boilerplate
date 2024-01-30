57a3e17546ee64cfc999811bac17e59539452dc5
ETERNALMOON <56554509+Hzegan@users.noreply.github.com>
2024-01-31 02:26:41 +0700

    init

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Button } from "@nextui-org/react";
+
+function App() {
+  return (
+    <div>
+      <h1>Hello</h1>
+      <Button color="primary">Button</Button>
+    </div>
+  );
+}
+
+export default App;
---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: "./",
+  build: {
+    outDir: "build",
+    rollupOptions: {
+      onwarn(warning, warn) {
+        if (warning.code === "MODULE_LEVEL_DIRECTIVE") {
+          return;
+        }
+        warn(warning);
+      },
+    },
+  },
+});
